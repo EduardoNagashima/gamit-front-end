fe5d7a84f57bbc074d4695e1dfa39cdc2a6f44cb
eduardoNagashima <eduyudji@gmail.com>
2022-08-18 10:17:35 -0300

    changing the local of click the post

--- gameit/src/services/api.js
+++ gameit/src/services/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 //MUDAR ISSO NA HORA DO DEPLOYs
 
-const api = axios.create({
-    baseURL: "http://localhost:5000",
-});
-
 // const api = axios.create({
-//     baseURL: "https://gameit-backend-deploy.herokuapp.com/",
+//     baseURL: "http://localhost:5000",
 // });
+
+const api = axios.create({
+    baseURL: "https://gameit-backend-deploy.herokuapp.com/",
+});
 
 export default api;
